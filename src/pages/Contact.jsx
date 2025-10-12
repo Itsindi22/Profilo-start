fd93dee08cae93e873cbb04f434869ed4f51ee15
Inderpreet <inderpreet2004123@gmail.com>
2025-10-11 21:52:53 -0400

    alert wip

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,34 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+
+
+import Loader from "../components/Loader";
+
+import Fox from "../models/Fox";
+import { Loader as DreiLoader } from "@react-three/drei"; // renamed to avoid conflict
+import Alert from "../components/Alert";
+import useAlert from "../hooks/useAlert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState ('idle')
+
+const {alert,showAlert,hideAlert} =useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleFocus = () => {};
-  const handleBlur = () => {};
-
+  const handleFocus = () =>  setCurrentAnimation('walk')
+  const handleBlur = () => setCurrentAnimation('idle');
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-
+    setCurrentAnimation('hit')
+  
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID, // ✅ fixed key name
@@ -32,17 +44,33 @@ const Contact = () => {
       )
       .then(() => {
         setIsLoading(false);
+        showAlert({show :true, text: 'Message sent successfully!',
+          type: "success"
+        })
         // TODO: SHOW success message
         // TODO: HIDE alert
-      })
-      .catch((error) => {
+
+        setTimeout(() => {
+          hideAlert();
+          setCurrentAnimation('idle')
+          setForm({name: '',email:'',message: ''})
+        },[3000])
+
+        setForm({name: '',email:'',message: ''})
+      }).catch((error) => {
         setIsLoading(false);
+        setCurrentAnimation('idle')
         console.log(error);
+        showAlert({ show: true, text:'I dint receive your message',
+          type: "danger"
+        })
       });
   };
 
   return (
     <section className="relative flex Lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert}/>}
+      
       <div className="flex-1 main-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -106,6 +134,30 @@ const Contact = () => {
             {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas camera={{ position: [0, 0, 5], 
+        fov: 75,
+        near: 0.01,
+        far: 1000
+        }}
+        >
+          <directionalLight intensity={2.5} position={[0,0,1]} />
+          <ambientLight intensity={0.5} />
+
+          <Suspense fallback={<Loader />}>
+            <Fox
+            currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+
+        {/* Drei Loader to show global progress */}
+        <DreiLoader />
       </div>
     </section>
   );
