f3f2f3136046dd74d4ab1f5d9127ef83f8709c56
Inderpreet <inderpreet2004123@gmail.com>
2025-10-15 13:10:44 -0400

    update about wip

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,74 @@
-import React  from "react";
+import { VerticalTimeline, VerticalTimelineElement }  from 
+'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+
+import { skills, experiences } from "../constants";
 
 const About = () => {
-    return (
-<section className= "Max-container">
-<h1 className="head-text">
-Hello,I'm<span className ="blue-gradient_text font-semibold
-drop-shadow">Inderpreet</span>
-</h1>
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Inderpreet
+        </span>{" "}
+        👋
+      </h1>
 
-<div>
-    <p> Full stack software Developer based in Indiana  specializing in technical eduction through  hands on learning and building applications.</p>
-</div>
-</section>  )
-    
-}
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Full stack developer based in Indiana, specializing in technical
+          education through hands-on learning and building applications.
+        </p>
+      </div>
 
-export default About
+      {/* Skills Section */}
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div key={skill.name} className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Experience Section */}
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>
+            I've worked with all sorts of companies, leveling up my skills and
+            teaming up with smart people. Here's the rundown.
+          </p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement>
+                <div>
+                  <h3>
+                    {experience.title}
+                    </h3>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
